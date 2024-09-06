5377cd8cc593ea40e638aa151fa533fabf2b3170
Tan Denis <tan.denis.td@gmail.com>
2024-09-06 10:08:24 +0200

    Ajout page mon compte

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Connexion", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
