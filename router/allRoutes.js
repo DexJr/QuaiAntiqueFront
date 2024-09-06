68c03ca49f4980b1062150ddcf8f69c2aea1670d
Tan Denis <tan.denis.td@gmail.com>
2024-09-06 09:56:35 +0200

    Ajout page connexion

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -4,6 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Connexion", "/pages/auth/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
